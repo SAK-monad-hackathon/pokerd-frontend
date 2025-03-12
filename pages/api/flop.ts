6d7caacbdcdf2538c3ca969f58ecd81051f5ba29
KirienzoEth <kirienzo.eth@gmail.com>
2025-03-12 23:21:54 +0100

    feat: add api calls for community cards

--- pages/api/flop.ts
+++ pages/api/flop.ts
@@ -3,16 +3,12 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export default async function flop(req: NextApiRequest, res: NextApiResponse) {
   req;
   try {
-    // Simulate
-    const flopData = [
-      { value: "Jack", suit: "Spade" },
-      { value: "Three", suit: "Heart" },
-      { value: "Five", suit: "Diamond" },
-    ];
-    console.log("flopData :", flopData);
+    const request = await fetch("https://pokerd-backend.fly.dev/flop");
+    const data = await request.json();
+    console.log("flopData :", data);
 
-    res.status(200).json(flopData);
+    res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: "Error flop server" });
+    res.status(500).json({ error: "Error river server" });
   }
 }
